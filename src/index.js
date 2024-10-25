c62991843f679f053ed0d528fd086a64278dc111
hendricwidjaja <hcwidjaja@gmail.com>
2024-10-26 10:19:02 +1100

    feat: Initialise our node environment and created simple function

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const { niceLogger } = require("./niceLogger.js"); // must remember relative path
+
+// Above is short form of below steps
+// 1. Import the File
+// const niceLoggerFile = require("./niceLogger.js"); 
+
+// 2. Import specific function from the file
+// const niceLoggerFunction = niceLoggerFile.niceLogger;
+
